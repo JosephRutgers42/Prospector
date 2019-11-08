622f61bbf146abd944846c5997258932f8804ca4
JosephRutgers42 <55005555+JosephRutgers42@users.noreply.github.com>
2019-11-08 01:03:13 -0500

    ScoreManager/Prospector Complete

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -169,6 +169,7 @@ public class Prospector : MonoBehaviour {
                 MoveToDiscard(target);
                 MoveToTarget(Draw());
                 UpdateDrawPile();
+                ScoreManager.EVENT(eScoreEvent.draw);
                 break;
 
             case eCardState.tableau:
@@ -183,6 +184,7 @@ public class Prospector : MonoBehaviour {
                 tableau.Remove(cd);
                 MoveToTarget(cd);
                 SetTableauFaces();
+                ScoreManager.EVENT(eScoreEvent.mine);
                 break;
         }
         CheckForGameOver();
@@ -209,9 +211,11 @@ public class Prospector : MonoBehaviour {
     void GameOver(bool won) {
         if (won)
         {
-            print("Game Over. You won! :)");
+            //print("Game Over. You won! :)");
+            ScoreManager.EVENT(eScoreEvent.gameWin);
         }else {
-            print("Game Over. You Lost. :(");
+            //print("Game Over. You Lost. :(");
+            ScoreManager.EVENT(eScoreEvent.gameLoss);
         }
         SceneManager.LoadScene("__Prospector_Scene_0");
     }
